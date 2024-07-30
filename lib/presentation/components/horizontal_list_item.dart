2b0c6f2ccd033228a17d854e32a864306a0e88c0
rescalon34 <roberto.escalon34@gmail.com>
2024-07-29 22:16:07 -0600

    💄 MOVF-redirection-flows - Create the UI for the Profile Screen.

---
+++ lib/presentation/components/horizontal_list_item.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HorizontalListItem extends StatelessWidget {
+  final String text;
+  final bool? showArrowIcon;
+  final Function() onItemClick;
+
+  const HorizontalListItem({
+    super.key,
+    required this.text,
+    this.showArrowIcon = true,
+    required this.onItemClick,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onItemClick,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 16, right: 0, top: 4),
+        child: Container(
+          color: Colors.transparent,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Text(
+                    text,
+                    style: Theme.of(context).textTheme.titleSmall,
+                  ),
+                  const Spacer(flex: 1),
+                  Visibility(
+                    visible: showArrowIcon == true,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 16),
+                      child: Icon(
+                        Icons.arrow_forward_ios_outlined,
+                        size: 13,
+                        color: Theme.of(context).colorScheme.onSurface,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              const Divider()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/presentation/pages/profile/profile_page.dart
+++ lib/presentation/pages/profile/profile_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movies_mobile_app_flutter/core/navigation/routes/profile_routes.dart';
+import 'package:gap/gap.dart';
 import 'package:movies_mobile_app_flutter/core/util/navigation_extensions.dart';
+import 'package:movies_mobile_app_flutter/data/util/string_extensions.dart';
 
-import '../../bloc/user_authentication/user_authentication_bloc.dart';
+import '../../../core/navigation/routes/profile_routes.dart';
+import '../../../data/util/network_constants.dart';
+import '../../components/circle_gradient_avatar.dart';
 import '../../components/custom_appbar.dart';
-import '../../components/elevated_large_button.dart';
+import '../../components/horizontal_list_item.dart';
+import '../../components/rounded_button.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -20,29 +23,70 @@ class ProfilePage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text("Profile"),
-            ),
+            const Gap(16),
+            _buildProfileHeader(),
+            const Gap(24),
+            _buildProfileOptions(context),
             Padding(
-              padding: const EdgeInsets.all(32),
-              child: ElevatedLargeButton(
-                text: "Go to Settings",
-                onClick: () => context.navigator.pushPage(SettingsAPageRoute()),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(32),
-              child: ElevatedLargeButton(
-                text: "Logout",
-                onClick: () {
-                  context.read<UserAuthenticationBloc>().add(const LoggedOut());
-                },
+              padding: const EdgeInsets.only(left: 16),
+              child: Text(
+                "Version 1.0.0",
+                style: Theme.of(context).textTheme.labelSmall,
               ),
             )
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildProfileHeader() {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          CircleGradientAvatar(
+            imageUrl: Constants.profileImage.getPosterUrl(
+              posterWidth: Constants.w150ProfileSize,
+            ),
+            imageSize: 45,
+          ),
+          const Gap(8),
+          RoundedButton(
+            text: "Edit Profile",
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProfileOptions(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        HorizontalListItem(
+          text: "App Settings",
+          onItemClick: () => context.navigator.pushPage(SettingsAPageRoute()),
+        ),
+        HorizontalListItem(
+          text: "Account Policy",
+          onItemClick: () => print("Account Policy click"),
+        ),
+        HorizontalListItem(
+          text: "Privacy Policy",
+          onItemClick: () => print("Privacy Policy click"),
+        ),
+        HorizontalListItem(
+          text: "Credits",
+          onItemClick: () => print("Credits click"),
+        ),
+        HorizontalListItem(
+          text: "Log out",
+          showArrowIcon: false,
+          onItemClick: () => print("Logout click"),
+        ),
+      ],
     );
   }
 }
